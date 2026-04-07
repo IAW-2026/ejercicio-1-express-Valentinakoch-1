9f94b55febdd14b0dc6c1ab4e9955dd216cfcbd2
Valentina Koch <valentinakoch2024@gmail.com>
2026-04-07 19:22:58 -0300

    Nivel 1 - servidor de archivos estaticos

--- app.js
+++ app.js
@@ -1,25 +1,8 @@
-const express = require('express'); // Importar Express
-const app = express(); // Crear instancia de Express
+const express = require('express');
+const app = express();
 
-// Middleware
-app.use(express.json()); //permitir parsear JSON en el cuerpo de las solicitudes      
-app.use(express.urlencoded({ extended: true })); // permitir parsear datos de formularios html
-
-// ACA AGREGUE
-app.use(express.static('public')); // Servir archivos estáticos desde la carpeta 'public'
-
-// Rutas
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/public/index.html');
-});
-
-app.get('/acerca', (req, res) => {
-  res.sendFile(__dirname + '/public/acerca.html');
-});
-
-app.get('/contacto', (req, res) => {
-  res.sendFile(__dirname + '/public/contacto.html');
-});
+// Servir archivos estáticos desde la carpeta 'public'
+app.use(express.static('public'));
 
 // Middleware básico para manejo de errores
 app.use((err, req, res, next) => {
@@ -27,17 +10,9 @@ app.use((err, req, res, next) => {
   res.status(500).json({ error: '¡Algo salió mal!' });
 });
 
-// Iniciar servidor 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Servidor ejecutándose en el puerto ${PORT}`);
 });
-
-
-
-
-
-
-
 
 
